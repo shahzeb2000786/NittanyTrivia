b5584563053a319454752a144bca5834f7d22552
Shahzeb Ahmed <shahzeb2000786@gmail.com>
2020-07-18 21:57:50 -0400

    Initial Commit

---
+++ NittanyTrivia/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  NittanyTrivia
+//
+//  Created by Shahzeb Ahmed on 7/18/20.
+//  Copyright © 2020 Shahzeb Ahmed. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+

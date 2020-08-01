e423eaee68761979037ecacfe4d3ab559e19de1e
Shahzeb Ahmed <shahzeb2000786@gmail.com>
2020-08-01 01:02:30 -0400

    Added timer animation to show amount of time remaining to answer question

--- NittanyTrivia/Controller/PlayController.swift
+++ NittanyTrivia/Controller/PlayController.swift
@@ -14,31 +14,44 @@ class PlayController: UIViewController {
     
     let questionChecker = QuestionChecker()//contains logic to check answers to questions
    
+    var timeToDisplay = 15//the amount of time user has to answerquestion
+    
     @IBOutlet weak var question: UITextView!
     @IBOutlet weak var firstOption: UIButton!
     @IBOutlet weak var secondOption: UIButton!
     @IBOutlet weak var thirdOption: UIButton!
     @IBOutlet weak var fourthOption: UIButton!
+    @IBOutlet weak var timerText: UILabel!
+    
     
     
     override func viewDidLoad() {
         super.viewDidLoad()
+            
+        var timerValue = 0//will be used to show the current time left for user to answer question
+        Timer.scheduledTimer(withTimeInterval: 1, repeats: true) { (timer) in
+            if self.timeToDisplay == 0{
+                timer.invalidate()
+                self.timerText.text = "Time has ended"
+            }
+            else{
+                self.timeToDisplay -= 1
+                self.timerText.text? = String(self.timeToDisplay)
+            }
+        }//end of scheduled timer
         
-            self.question.text? = questionToAskUser.question
-            self.firstOption.setTitle(questionToAskUser.option1, for: .normal)
-            self.secondOption.setTitle(questionToAskUser.option2, for: .normal)
-            self.thirdOption.setTitle(questionToAskUser.option3, for: .normal)
-            self.fourthOption.setTitle(questionToAskUser.option4, for: .normal)
+        
+        self.question.text? = questionToAskUser.question
+        self.firstOption.setTitle(questionToAskUser.option1, for: .normal)
+        self.secondOption.setTitle(questionToAskUser.option2, for: .normal)
+        self.thirdOption.setTitle(questionToAskUser.option3, for: .normal)
+        self.fourthOption.setTitle(questionToAskUser.option4, for: .normal)
         
         self.question.layer.cornerRadius = 20
         self.firstOption.layer.cornerRadius = 40
-
         self.secondOption.layer.cornerRadius = 40
         self.thirdOption.layer.cornerRadius = 40
         self.fourthOption.layer.cornerRadius = 40
-
-        
-        
     }
     
     

082888b17daefa7fa719cbfd6edad2d7f3eac161
Shahzeb Ahmed <shahzeb2000786@gmail.com>
2020-08-05 00:16:41 -0400

    Google sign in segue fixed and added segue to homepage

--- NittanyTrivia/Controller/WelcomeViewController.swift
+++ NittanyTrivia/Controller/WelcomeViewController.swift
@@ -11,7 +11,8 @@ import Firebase
 import GoogleSignIn
 
 
-class WelcomeViewController: UIViewController {
+
+class WelcomeViewController: UIViewController, GIDSignInDelegate {
 
     @IBOutlet weak var googleButton: GIDSignInButton!
     
@@ -20,12 +21,35 @@ class WelcomeViewController: UIViewController {
         super.viewDidLoad()
         GIDSignIn.sharedInstance()?.presentingViewController = self
         // Automatically sign in the user.
+        GIDSignIn.sharedInstance().delegate = self
+        
         GIDSignIn.sharedInstance()?.restorePreviousSignIn()
         googleButton.layer.cornerRadius = 20
         googleButton.sizeToFit()
        // print(setUserInfo())
-        
+       
     }
     
-
+    
+    func sign(_ signIn: GIDSignIn!, didSignInFor user: GIDGoogleUser!, withError error: Error?) {
+         
+         if let error = error {
+           return
+         }
+           print("successful sign in")
+         guard let authentication = user.authentication else { return }
+         let credential = GoogleAuthProvider.credential(withIDToken: authentication.idToken,
+                                                           accessToken: authentication.accessToken)
+  
+         let idToken = user.authentication.idToken // Safe to send to the server
+           Auth.auth().signIn(with: credential) { (User, Error) in    //authenticates user's sign in info
+               if let userInfo = User {
+                   print("howdy there")
+                   self.performSegue(withIdentifier: "toHomePage", sender: self)
+               }
+               else{
+                   print("not successful")
+               }
+           }
+       } // google sign in method
 }

d922da56ef27a42351adbad07f44f9bf526e657b
Shahzeb Ahmed <shahzeb2000786@gmail.com>
2020-08-07 15:57:00 -0400

    basic leaderboard now displays top five users

--- NittanyTrivia/Controller/Leaderboard.swift
+++ NittanyTrivia/Controller/Leaderboard.swift
@@ -10,30 +10,62 @@ import Foundation
 import UIKit
 import Firebase
 
+
 class Leaderboard: UIViewController {
 
-    
+    @IBOutlet weak var firstPlace: UILabel!
+    @IBOutlet weak var secondPlace: UILabel!
+    @IBOutlet weak var thirdPlace: UILabel!
+    @IBOutlet weak var fourthPlace: UILabel!
+    @IBOutlet weak var fifthPlace: UILabel!
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        firstPlace.layer.cornerRadius = 40
+        secondPlace.layer.cornerRadius = 40
+        thirdPlace.layer.cornerRadius = 40
+        fourthPlace.layer.cornerRadius = 40
+        fifthPlace.layer.cornerRadius = 40
+        
+        firstPlace.adjustsFontSizeToFitWidth = true
+        var leaderBoardLabels = [firstPlace!, secondPlace!,thirdPlace!,fourthPlace!,fifthPlace!]
+
         var leaderboardRef = db.collection("Users")
             .order(by: "points", descending: true)
             .limit(to: 5)
-        
         leaderboardRef.getDocuments { (querySnapshot, err) in
             print(querySnapshot)
             if let error = err{
                 print(err)
             }//end of if
             else{
+                var topFivePlayers = Array<Any>()
                 if let querySnapshot = querySnapshot {
                     for document in querySnapshot.documents{
-                        print(document.data())
+                        topFivePlayers.append(document.data()["email"])
+                        print(document.data()["email"])
+                         print(document.data()["points"])
+                        print(document.data().count)
                     }//end of for
+                    DispatchQueue.main.async {
+                        self.setLeaderboard(players: topFivePlayers, leaderBoardLabels: leaderBoardLabels)
+                    }//end of DispatchQueue
                 }//end of if optional binding
             }//end of else
         }//end of getDocuments
     }//end of viewDidLoad
     
 
+}
+
+//extension below deals with setting the leaderboard values
+extension Leaderboard {
+    //setLeaderboard sets the leaderboard label values and is executed inside the view didload of the view controller
+    func setLeaderboard(players: Array<Any>, leaderBoardLabels: [UILabel]){
+        let numberOfPlayers = players.count
+        for playerNumber in 0...players.count-1{
+            leaderBoardLabels[playerNumber].text = players[playerNumber] as! String
+        }
+    }
 }

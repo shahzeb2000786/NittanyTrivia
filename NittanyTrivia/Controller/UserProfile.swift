86bfd49d0d8fa8de9da6aa196e62ba5a8480a88f
Shahzeb Ahmed <shahzeb2000786@gmail.com>
2020-09-10 23:31:18 -0400

    Panel view now looks the same size across all current pages in app

--- NittanyTrivia/Controller/UserProfile.swift
+++ NittanyTrivia/Controller/UserProfile.swift
@@ -18,20 +18,17 @@ class UserProfile: UIViewController{
     @IBOutlet weak var drawsLabel: UILabel!
     @IBOutlet weak var pointsLabel: UILabel!
     @IBOutlet weak var winPercentage: UIProgressView!
-    @IBOutlet weak var coinsLabel: UILabel!
-    @IBOutlet weak var gemsLabel: UILabel!
+ 
     
     
     @IBOutlet weak var pointsView: UIView!
-    @IBOutlet weak var coinsView: UIView!
-    @IBOutlet weak var gemsView: UIView!
+
     
     
     override func viewDidLoad(){
         super.viewDidLoad()
         pointsView.layer.cornerRadius = 40
-        coinsView.layer.cornerRadius = 20
-        gemsView.layer.cornerRadius = 20
+
         
         getUserProfileInfo()//changes global profile variable, which is located in User.swift
         print(currentUserProfile.winPercentage)
@@ -42,8 +39,8 @@ class UserProfile: UIViewController{
             self.drawsLabel.text = String(currentUserProfile.draws)
             self.pointsLabel.text = String(currentUserProfile.points)
             self.winPercentage.progress = currentUserProfile.winPercentage
-            self.coinsLabel.text = String(currentUserProfile.coins) + "💰"
-            self.gemsLabel.text = String(currentUserProfile.gems) + "💎"
+//            self.coinsLabel.text = String(currentUserProfile.coins) + "💰"
+//            self.gemsLabel.text = String(currentUserProfile.gems) + "💎"
         }//DispatchQueue
     }//viewDidLoad
 }//UserProfile
